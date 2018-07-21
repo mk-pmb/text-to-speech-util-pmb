6cd0daed928bcb40d39868f7c2e21583bea1d12d
mk-pmb <mk-pmb@users.noreply.github.com>
2018-07-21 18:49:36 +0200

    v0.1.8: stop reading before reading next text

--- src/netcat_server.lib.sh
+++ src/netcat_server.lib.sh
@@ -28,20 +28,25 @@ function netcat_server__one_turn () {
     MSG="${MSG#*$'\n'}"
   fi
 
+  if [ -z "$MSG" ]; then
+    echo "D: empty message. ignored."
+    return 0
+  fi
+
+  echo "gonna stop reading"
+  <<<"$MSG" vengmgr lang:'*' speak_stop || return $?$(
+    echo "E: failed to shut up, rv=$?" >&2)
+
   case "$MSG" in
-    '' ) ;;
     *[A-Za-z0-9]* )
       [ -n "$LNG" ] || LNG="$(<<<"$MSG" guess_text_lang)"
       echo "gonna read as lang:$LNG."
       <<<"$MSG" vengmgr lang:"$LNG" speak_stdin || return $?$(
         echo "E: failed to speak, rv=$?" >&2)
       ;;
-    * )
-      echo "gonna stop reading"
-      <<<"$MSG" vengmgr lang:'*' speak_stop || return $?$(
-        echo "E: failed to shut up, rv=$?" >&2)
-      ;;
+    * ) echo "D: message with no letters or digits. ignored.";;
   esac
+  sleep 2s
   return 0
 }
 
