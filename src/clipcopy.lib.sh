d1d5a8fa71b88f7aa7ed50daba64f0cfda9758bb
mk-pmb <mk-pmb@users.noreply.github.com>
2021-09-24 06:52:43 +0200

    v0.2.6: Make clipcopy mode configurable

--- src/clipcopy.lib.sh
+++ src/clipcopy.lib.sh
@@ -2,18 +2,44 @@
 # -*- coding: utf-8, tab-width: 2 -*-
 
 function clipcopy () {
+  local CC_FROM=
+  clipcopy_guess_from || return $?
+  clipcopy_from_"$CC_FROM" || return $?
+}
+
+
+function clipcopy_guess_from () {
   # Determine active program in order to decide how to extract text:
   local AWIN_WID="$(xdotool getactivewindow)"
   local AWIN_PID="$(xdotool getwindowpid "$AWIN_WID")"
   local AWIN_CLS="$(wmctrl -xl | grep -oPe '^(\S+\s+){3}' \
     | sed -nre 's~\s+$~~;s~^0x0*'"$(printf %x "$AWIN_WID")"' +\S+ ~~p')"
+  local AWIN_NAME="${AWIN_CLS%%.*}"
+  AWIN_CLS="${AWIN_CLS#*.}"
   local AWIN_PROG=
   # [ -z "$AWIN_PID" ] || AWIN_PROG="$(ps ho comm "$AWIN_PID")"
   # enus gxmessage "$(local -p)"
-  case "$AWIN_CLS" in
-    dillo.Dillo | \
-    __middle_click_paste_style__ ) LANG=C xsel --output --primary; return $?;;
-  esac
+
+  for CC_FROM in "$AWIN_NAME.$AWIN_CLS" "$AWIN_NAME." ".$AWIN_CLS"; do
+    CC_FROM="${TTS[clipcopy:mode_by_winclass:$CC_FROM]}"
+    [ -n "$CC_FROM" ] && return 0
+  done
+
+  CC_FROM='clipboard'
+}
+
+
+function clipcopy_from_primary () {
+  # The assumption for the "primary" buffer is that it already holds
+  # the data, due to a user having made a selection.
+  LANG=C xsel --output --primary
+}
+
+
+function clipcopy_from_clipboard () {
+  # The assumption for the "clipboard" buffer is that a user's selection
+  # has not caused the text to be copied yet, and instead we should make
+  # the active window copy the selection.
 
   local CLIP_BACKUP="$(xsel --clipboard --output | base64)"
   local CLIP_TEXT=
